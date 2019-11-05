fa1afc1f202b3960893783a7b77a4f9cfee48a1a
Peter Cole <petercole3@gmail.com>
2019-11-05 10:02:12 -0800

    Update class07 branch

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-###LAB 6 - SALMON COOKIES
+#LAB 6 - SALMON COOKIES
 
 _v0 11/4/19_
 Created root
 Updated README.md with actual Markdown text
 Got outline of what's needed in js and both pages
+
+_v1 11/5/19_
+Start on constructors
--- js/app.js
+++ js/app.js
@@ -1,21 +1,62 @@
 'use strict';
 
-
 // Stores the min/max hourly customers, and the average cookies per customer,
 //     in object properties
 
-var seattleData = {
-    
-}
+var seattle = {
+    name: 'Seattle',
+    minSeattle: '23',
+    maxSeattle: '65',
+    avgCookieSeattle: '6.3',
+    randCustomer: function() {
+        return Math.floor(Math.random() * Math.floor(11));
+    }
+};
+
+var tokyo = {
+    name: 'Tokyo',
+    minTokyo: '3',
+    maxTokyo: '24',
+    avgCookieTokyo: '1.2'
+};
+
+var dubai = {
+    name: 'Dubai',
+    minDubai: '11',
+    maxDubai: '38',
+    avgCookieDubai: '3.7'
+};
+
+var paris = {
+    name: 'Paris',
+    minParis: '20',
+    maxParis: '38',
+    avgCookieParis: '2.3'
+};
+var lima = {
+    name: 'Lima',
+    minLima: '2',
+    maxLima: '16',
+    avgCookieLima: '4.6'
+};
 
 // Uses a method of that object to generate a random number of customers per hour.
 //     Objects/Math/random
+
+/*
+randCustomer: function() {
+  return Math.floor(Math.random() * Math.floor(11));
+}
+*/
 
 // Calculate and store the simulated amounts of cookies purchased for each hour
 //     at each location using average cookies purchased
 //     and the random number of customers generated
 
-// Store the results for each location in a separate array… 
+var storeHours = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am',
+    '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', ];
+
+// Store the results for each location in a separate array…
 //     perhaps as a property of the object representing that location
 
 // Display the values of each array as unordered lists in the browser
