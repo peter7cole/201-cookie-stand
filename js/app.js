dea8665a8a3029f843364dff3a9d1e43c37aa02f
Peter Cole <petercole3@gmail.com>
2019-11-05 08:57:51 -0800

    Upload of first day's prep

--- js/app.js
+++ js/app.js
@@ -1,45 +1,48 @@
 'use strict';
 
-/*
-Stores the min/max hourly customers, and the average cookies per customer,
-    in object properties
 
-Uses a method of that object to generate a random number of customers per hour.
-    Objects/Math/random
+// Stores the min/max hourly customers, and the average cookies per customer,
+//     in object properties
 
-Calculate and store the simulated amounts of cookies purchased for each hour
-    at each location using average cookies purchased
-    and the random number of customers generated
+var seattleData = {
+    
+}
 
-Store the results for each location in a separate array… 
-    perhaps as a property of the object representing that location
+// Uses a method of that object to generate a random number of customers per hour.
+//     Objects/Math/random
 
-Display the values of each array as unordered lists in the browser
+// Calculate and store the simulated amounts of cookies purchased for each hour
+//     at each location using average cookies purchased
+//     and the random number of customers generated
 
-Calculating the sum of these hourly totals;
-your output for each location should look like this:
+// Store the results for each location in a separate array… 
+//     perhaps as a property of the object representing that location
 
-Seattle
+// Display the values of each array as unordered lists in the browser
 
-6am: 16 cookies
-7am: 20 cookies
-8am: 35 cookies
-9am: 48 cookies
-10am: 56 cookies
-11am: 77 cookies
-12pm: 93 cookies
-1pm: 144 cookies
-2pm: 119 cookies
-3pm: 84 cookies
-4pm: 61 cookies
-5pm: 23 cookies
-6pm: 42 cookies
-7pm: 57 cookies
-Total: 875 cookies
+// Calculating the sum of these hourly totals;
+// your output for each location should look like this:
 
-Display the lists on sales.html
+// Seattle
 
-*/
+// 6am: 16 cookies
+// 7am: 20 cookies
+// 8am: 35 cookies
+// 9am: 48 cookies
+// 10am: 56 cookies
+// 11am: 77 cookies
+// 12pm: 93 cookies
+// 1pm: 144 cookies
+// 2pm: 119 cookies
+// 3pm: 84 cookies
+// 4pm: 61 cookies
+// 5pm: 23 cookies
+// 6pm: 42 cookies
+// 7pm: 57 cookies
+// Total: 875 cookies
+
+// Display the lists on sales.html
+
 
 var dataDiv = document.getElementById('data');
 
